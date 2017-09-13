b21349de2db715ebc1891aa8e5274c06538db168
Weicheng Yu <wy248@en-cs-totient-01.coecis.cornell.edu>
2017-09-13 16:15:25 -0400

    check out new timing

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,8 +1,19 @@
+#include <math.h>
+
 const char* dgemm_desc = "My awesome dgemm.";
 
 void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
-    int i, j, k;
+	int j,k,i;
+	for (j = 0; j < M; ++j) {
+        for (k = 0; k < M; ++k) {
+            for (i = 0; i < M; ++i) {
+                C[j*M+i] += A[k*M+i] * B[j*M+k];
+            }
+        }
+    }
+/*						
+   int i, j, k;
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
             double cij = C[j*M+i];
@@ -11,4 +22,26 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
             C[j*M+i] = cij;
         }
     }
+  
+int i, k, j;
+    
+    for (i = 0; i < M; ++i) {
+        for (k = 0; k < M; ++k) {
+            for (j = 0; j < M; ++j) {
+                C[j*M+i] += A[k*M+i] * B[j*M+k];
+            }
+        }
+    }
+  
+      int j, i, k;
+    for (j = 0; j < M; ++j) {
+        for (i = 0; i < M; ++i) {
+            double cij = C[j*M+i];
+            for (k = 0; k < M; ++k)
+                cij += A[k*M+i] * B[j*M+k];
+            C[j*M+i] = cij;
+        }
+    }	*/
 }
+
+
