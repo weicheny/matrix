d7875f22c7b826bd7d14e491787053f9bf63b992
Weicheng Yu <wy248@en-cs-totient-01.coecis.cornell.edu>
2017-09-13 21:07:52 -0400

    size128

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,7 +1,7 @@
-const char* dgemm_desc = "Simple blocked dgemm.";
+const char* dgemm_desc = "mine dgemm.";
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 16)
+#define BLOCK_SIZE ((int) 128)
 #endif
 
 /*
